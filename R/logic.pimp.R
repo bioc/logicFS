474f5569408a4707ec06791d3529c7b673871c21
Ting-Yuan Liu <tliu@fhcrc.org>
2006-04-05 16:41:28 +0000

    Add 2 new packages:  logicFS & OrderedList
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/logicFS@17038 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/logic.pimp.R
@@ -0,0 +1,11 @@
+logic.pimp<-function(log.out){
+	if(!is(log.out,"logicBagg"))
+		stop("log.out must be an object of class logicBagg.")
+	lmodel<-log.out$logreg.model
+	n.lmodel<-length(lmodel)
+	list.primes<-vector("list",n.lmodel)
+	for(i in 1:n.lmodel)
+		list.primes[[i]]<-lapply(lmodel[[i]]$trees,getPImps)
+	list.primes
+}
+
---
+++ R/vim.multiple.R
@@ -0,0 +1,26 @@
+vim.multiple<-function(mprimes,mat.eval,inbagg,cl,prob.case=0.5){
+	primes<-unique(unlist(mprimes))
+	n.trees<-length(mprimes)
+	n.primes<-length(primes)
+	mat.in<-cbind(1-diag(n.primes),1)
+	rownames(mat.in)<-primes		
+	list.eval<-lapply(mprimes,function(x,e=mat.eval,i=mat.in) 
+		e[,x,drop=FALSE]%*%i[x,,drop=FALSE]>0)
+	vec.improve<-numeric(n.primes+1)
+	getIth<-function(x,ids=NULL) x[,ids]
+	oob<-which(!(1:nrow(mat.eval))%in%inbagg)
+	for(i in 1:(n.primes+1)){
+		mat.model<-matrix(unlist(lapply(list.eval,getIth,ids=i)),ncol=n.trees)
+		if(any(colSums(mat.model)==0))
+			mat.model<-mat.model[,colSums(mat.model)>0]
+		mat.model<-data.frame(cl=cl,mat.model)
+		glm.out<-glm(cl~.,data=mat.model[inbagg,],family="binomial")
+		preds<-predict(glm.out,mat.model[oob,],type="response")>prob.case
+		vec.improve[i]<-sum(preds==cl[oob])
+	}
+	vec.improve<-vec.improve[n.primes+1]-vec.improve
+	vec.improve<-vec.improve[-(n.primes+1)]
+	names(vec.improve)<-primes
+	vec.improve
+}
+
