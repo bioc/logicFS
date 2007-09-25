fc9c0fd7959ad31d1368e91bef6fd7935d0926b0
Holger Schwender <holger.schwender@uni-dortmund.de>
2007-09-25 13:56:35 +0000

    Fixed a bug in vim.norm.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/logicFS@27495 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/vim.norm.R
+++ R/vim.norm.R
@@ -7,7 +7,7 @@ vim.norm<-function(object,mu=0){
 	names(stat)<-rownames(mat.imp)
 	vim$vim<-stat
 	vim$measure<-"Standardized"
-	vim$threshold<-qt(1-0.05/nrow(mat.imp),ncol(mat.imp))
+	vim$threshold<-qt(1-0.05/nrow(mat.imp),ncol(mat.imp)-1)
 	vim$mu<-mu
 	vim
 }
